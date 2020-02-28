ca8fb8ec763f01523f9b2c9acd47c6248038eb6e
Josemar Luedke <josemarluedke@gmail.com>
2020-02-28 12:19:29 -0800

    Upgrade to Ember Octane

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,10 +1,10 @@
 import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
 import config from './config/environment';
 
-const Router = AddonDocsRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+class Router extends AddonDocsRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
   docsRoute(this, function() {
